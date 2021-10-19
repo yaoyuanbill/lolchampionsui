4fce8cec143a2f85617a71f721ea508147d3ba18
Darryl Greig <darryl.greig@owerya.com>
2021-10-19 16:28:20 +0100

    added apiUrl to the production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://franks-lolchampion-demo-franks-lolchampion-demo.emeadocker65.conygre.com/api/v1/lolchampion/"
 };
