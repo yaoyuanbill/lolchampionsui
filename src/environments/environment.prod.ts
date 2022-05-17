0abd5b288ef7436f183e532d0aee0911d2e875de
Owerya Resourcing <oweryaresourcing@gmail.com>
2022-05-17 12:17:46 +0000

    Now I have changed the URL for openshift to servicename.clustername.svc:8080/api/v1/lolchampion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "lol-champions-dsg-jpa.svc:8080/api/v1/lolchampion/"
+  apiUrl: "lol-champions-dsg-jpa.admin-lolchampions.svc:8080/api/v1/lolchampion/"
 };
