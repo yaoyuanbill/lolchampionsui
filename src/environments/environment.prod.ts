1501c4f97d77581c388481dc9e6babbe2ecefe15
Owerya Resourcing <oweryaresourcing@gmail.com>
2022-05-18 08:39:58 +0000

    environment.prod.ts edited online with Bitbucket different workspace

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://lol-champions-dsg-jpa-admin-lolchampions.apps.ose.openshift.conygre.com/api/v1/lolchampion"
+  apiUrl: "https://lol-champions-ose10-lol.apps.ose.openshift.conygre.com/api/v1/lolchampion"
 };
