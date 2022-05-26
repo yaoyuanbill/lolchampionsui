0cedf441cb960d9562bcbd8161ee7f22b7f25484
yaoyuanbill <yaoyuan.bill@gmail.com>
2022-05-26 11:43:54 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://lol-champions-ose10-lol.apps.ose.openshift.conygre.com/api/v1/lolchampion"
+  apiUrl: "https://lol-champions-ose14-champs.apps.ose.openshift.conygre.com/api/v1/lolchampion"
 };
